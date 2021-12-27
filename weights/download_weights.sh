0f85e4f3444c41cef5c600257deb98aade939476
Liu Changyu <liuchangyu1111@gmail.com>
2021-12-27 15:05:31 +0800

    Delete weights directory

--- weights/download_weights.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Start downloading pre training model..."
-wget https://github.com/Lornatang/EDSR-PyTorch/releases/download/1.0/edsr_2x.pth
-wget https://github.com/Lornatang/EDSR-PyTorch/releases/download/1.0/edsr_4x.pth
-echo "All pre training models have been downloaded!"
