274d23db84fbc4ec81596b41ecef7c8e1a3e2917
Lorna <liuchangyu1111@gmail.com>
2022-01-12 13:39:40 +0800

    Update run.py

--- scripts/run.py
+++ scripts/run.py
@@ -1,5 +1,5 @@
 import os
 
-# Prepare single scale dataset
-os.system("python ./prepare_dataset.py --images_dir ../data/DIV2K/original/train --output_dir ../data/DIV2K/EDSR/train")
-os.system("python ./prepare_dataset.py --images_dir ../data/DIV2K/original/valid --output_dir ../data/DIV2K/EDSR/valid")
+# Prepare dataset
+os.system("python ./prepare_dataset.py --images_dir ../data/DIV2K/original/train --output_dir ../data/DIV2K/EDSR/train --image_size 216 --step 108 --num_workers 10")
+os.system("python ./prepare_dataset.py --images_dir ../data/DIV2K/original/valid --output_dir ../data/DIV2K/EDSR/valid --image_size 216 --step 108 --num_workers 10")
